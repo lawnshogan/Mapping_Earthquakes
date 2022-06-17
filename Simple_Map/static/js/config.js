bc0fe6a03865c2d322b316674d53d45434e939ec
Shawn Logan <99139544+lawnshogan@users.noreply.github.com>
2022-06-17 15:16:44 -0600

    Delete config.js

--- Simple_Map/static/js/config.js
+++
@@ -1,4 +0,0 @@
-// API Key
-// Open the command line (Terminal or Git Bash) and type the following: python -m http.server
-// localhost:8000
-const API_KEY = "pk.eyJ1IjoibGF3bnNob2dhbiIsImEiOiJjbDRkMXUzeHAwMnQ3M29xeXV0bHJpa2I2In0.e7hSnGJEXtbZzYf9etNVWg";
